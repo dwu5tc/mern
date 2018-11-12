f0a26c74ab2214e5f8b50d32e3725449f04dee00
Dean Wu <dean.wu55@gmail.com>
2018-11-11 22:06:12 -0500

    complete l50. createprofile component

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/createProfile/CreateProfile';
 
 import './App.css';
 
@@ -48,6 +49,7 @@ class App extends Component {
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
               <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+              <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
             </div>
             <Footer />
           </div>
